0348f9d38c7cf09e46449d993a64cdd3527aec43
Aleksey <48358627+AlekseyFedorov27@users.noreply.github.com>
2020-01-20 17:53:26 +0300

    Add files via upload

---
+++ dist/precache-manifest.66233f2ca60406202ca4af969cf978a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a5103275dc57832312a1",
+    "url": "/js/chunk-vendors.932582f6.js"
+  },
+  {
+    "revision": "444e34e2062cc7e15425",
+    "url": "/js/app.54933d93.js"
+  },
+  {
+    "revision": "208c24a5590c359c61e5e34c365b30cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5103275dc57832312a1",
+    "url": "/css/chunk-vendors.33fbaed9.css"
+  },
+  {
+    "revision": "444e34e2062cc7e15425",
+    "url": "/css/app.60bb75dd.css"
+  }
+];
